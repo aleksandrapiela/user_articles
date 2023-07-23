1cbfdca441281980e9df526b3b06d190f79b0a8b
Ola Piela <apielaart@gmail.com>
2023-07-23 22:23:11 +0200

    Add retrofit

--- lib/domain/models/article_model.dart
+++ lib/domain/models/article_model.dart
@@ -1,28 +1,16 @@
-// import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-// part 'article_model.g.dart';
+part 'article_model.freezed.dart';
+part 'article_model.g.dart';
 
-// @JsonSerializable()
-class ArticleModel {
-  const ArticleModel({
-    required this.id,
-    required this.authorId,
-    required this.content,
-  });
+@freezed
+class ArticleModel with _$ArticleModel {
+  factory ArticleModel(
+    int id,
+    @JsonKey(name: 'author_id') int authorId,
+    String content,
+  ) = _ArticleModel;
 
-  final int id;
-
-  // @JsonKey(name: 'author_Id')
-  final int authorId;
-
-  final String content;
-
-  // factory ArticleModel.fromJson(Map<String, dynamic> json) =>
-  //     _$ArticleModelFromJson(json);
-
-  // Map<String, dynamic> toJson() => _$ArticleModelToJson(this);
-  ArticleModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        authorId = json['author_id'],
-        content = json['content'];
+  factory ArticleModel.fromJson(Map<String, dynamic> json) =>
+      _$ArticleModelFromJson(json);
 }
